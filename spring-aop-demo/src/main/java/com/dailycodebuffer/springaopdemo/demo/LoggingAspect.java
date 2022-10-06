f693f9478ff76235847238831c31d8df937b6663
lalit <lalit@lalits-MacBook-Pro.local>
2022-10-06 16:25:25 +0530

    Accessing the arguments in the Aspect class functions

--- spring-aop-demo/src/main/java/com/dailycodebuffer/springaopdemo/demo/LoggingAspect.java
+++ spring-aop-demo/src/main/java/com/dailycodebuffer/springaopdemo/demo/LoggingAspect.java
@@ -1,4 +1,5 @@
 package com.dailycodebuffer.springaopdemo.demo;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -9,12 +10,18 @@ import org.springframework.stereotype.Component;
 public class LoggingAspect {
     // * can be replaced with the return type of the function(void in this case)
     @Before("execution(* com.dailycodebuffer.springaopdemo.demo.ShoppingCart.checkOut(..))")
-    public void beforeLogger(){
-        System.out.println("Before Loggers!");
+    public void beforeLogger(JoinPoint joinPoint){
+//        System.out.println(joinPoint.getSignature());
+//        System.out.println(joinPoint.getArgs()[0]);
+        String arg = joinPoint.getArgs()[0].toString();
+        System.out.println("Before Loggers! with argument : " + arg);
     }
 //    @After("execution(* com.dailycodebuffer.springaopdemo.demo.ShoppingCart.checkOut(..))")
     @After("execution(* *.*.*.*.*.checkOut(..))") // first * == "Any return type" 2nd * == "com" and 3rd * == "dailycodebuffer" 4th * == "springaopdemo" 5th * == "demo" and 6th * == "ShoppingCart"
-    public void afterLogger(){
-        System.out.println("After Loggers!");
+    public void afterLogger(JoinPoint joinPoint){
+//        System.out.println(joinPoint.getSignature());
+//        System.out.println(joinPoint.getArgs()[0]);
+        String arg = joinPoint.getArgs()[0].toString();
+        System.out.println("After Loggers! with argument : " + arg);
     }
 }
