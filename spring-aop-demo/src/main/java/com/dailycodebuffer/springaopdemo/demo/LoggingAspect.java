c7c806e7aa26b19739c2b3fc63fe83da08d144a4
lalit <lalit@lalits-MacBook-Pro.local>
2022-10-06 15:33:13 +0530

    Defining the point cuts(Means which method is executed and when(Loggers)). Use @Before and @After

--- spring-aop-demo/src/main/java/com/dailycodebuffer/springaopdemo/demo/LoggingAspect.java
+++ spring-aop-demo/src/main/java/com/dailycodebuffer/springaopdemo/demo/LoggingAspect.java
@@ -1,4 +1,5 @@
 package com.dailycodebuffer.springaopdemo.demo;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -7,8 +8,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class LoggingAspect {
     // * can be replaced with the return type of the function(void in this case)
-    @Before("execution(* com.dailycodebuffer.springaopdemo.demo.ShoppingCart.checkOut())")
-    public void logger(){
-        System.out.println("Loggers!");
+    @Before("execution(* com.dailycodebuffer.springaopdemo.demo.ShoppingCart.checkOut(..))")
+    public void beforeLogger(){
+        System.out.println("Before Loggers!");
+    }
+//    @After("execution(* com.dailycodebuffer.springaopdemo.demo.ShoppingCart.checkOut(..))")
+    @After("execution(* *.*.*.*.*.checkOut(..))") // first * == "Any return type" 2nd * == "com" and 3rd * == "dailycodebuffer" 4th * == "springaopdemo" 5th * == "demo" and 6th * == "ShoppingCart"
+    public void afterLogger(){
+        System.out.println("After Loggers!");
     }
 }
--- spring-aop-demo/src/main/java/com/dailycodebuffer/springaopdemo/demo/ShoppingCart.java
+++ spring-aop-demo/src/main/java/com/dailycodebuffer/springaopdemo/demo/ShoppingCart.java
@@ -3,7 +3,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ShoppingCart {
-    public void checkOut(){
+    public void checkOut(String status){
         // These are the cross cutting concerns (we need to separate these from the business logic)
         // Logging
         // Authentication & Authorization
