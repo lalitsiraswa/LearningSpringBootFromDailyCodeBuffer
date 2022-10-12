2bb81907c5489557aae3c5b23db213421542f9ce
lalit <lalit@lalits-MacBook-Pro.local>
2022-10-12 12:47:27 +0530

    Create fetchDepartmentList API

--- Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboot/tutorial/controller/DepartmentController.java
+++ Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboot/tutorial/controller/DepartmentController.java
@@ -1,9 +1,12 @@
 package com.dailycodebuffer.Springboot.tutorial.controller;
 import com.dailycodebuffer.Springboot.tutorial.entity.Department;
+import com.dailycodebuffer.Springboot.tutorial.repository.DepartmentRepository;
 import com.dailycodebuffer.Springboot.tutorial.service.DepartmentService;
 import com.dailycodebuffer.Springboot.tutorial.service.DepartmentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class DepartmentController {
@@ -13,5 +16,9 @@ public class DepartmentController {
     @PostMapping("/departments")
     public Department saveDepartment(@RequestBody Department department){
         return departmentService.saveDepartment(department);
+    }
+    @GetMapping("/departments")
+    public List<Department> fetchDepartmentList(){
+        return departmentService.fetchDepartmentList();
     }
 }
--- Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboot/tutorial/service/DepartmentServiceImpl.java
+++ Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboot/tutorial/service/DepartmentServiceImpl.java
@@ -4,6 +4,8 @@ import com.dailycodebuffer.Springboot.tutorial.repository.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class DepartmentServiceImpl implements DepartmentService{
     @Autowired
@@ -11,5 +13,10 @@ public class DepartmentServiceImpl implements DepartmentService{
     @Override
     public Department saveDepartment(Department department) {
         return departmentRepository.save(department);
+    }
+
+    @Override
+    public List<Department> fetchDepartmentList() {
+        return departmentRepository.findAll();
     }
 }
