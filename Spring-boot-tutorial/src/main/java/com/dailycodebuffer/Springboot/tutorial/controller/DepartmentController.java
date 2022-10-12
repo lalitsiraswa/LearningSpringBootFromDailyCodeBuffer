48dbcf5718633ca91e91ef958a36a8e8f13745de
lalit <lalit@lalits-MacBook-Pro.local>
2022-10-12 12:05:22 +0530

    Create Department Save API

--- Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboot/tutorial/controller/DepartmentController.java
+++ Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboot/tutorial/controller/DepartmentController.java
@@ -1,7 +1,17 @@
 package com.dailycodebuffer.Springboot.tutorial.controller;
-import org.springframework.web.bind.annotation.RestController;
+import com.dailycodebuffer.Springboot.tutorial.entity.Department;
+import com.dailycodebuffer.Springboot.tutorial.service.DepartmentService;
+import com.dailycodebuffer.Springboot.tutorial.service.DepartmentServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class DepartmentController {
-
+    @Autowired
+    private DepartmentService departmentService;
+//    @RequestMapping(value = "/departments", method = RequestMethod.POST)
+    @PostMapping("/departments")
+    public Department saveDepartment(@RequestBody Department department){
+        return departmentService.saveDepartment(department);
+    }
 }
