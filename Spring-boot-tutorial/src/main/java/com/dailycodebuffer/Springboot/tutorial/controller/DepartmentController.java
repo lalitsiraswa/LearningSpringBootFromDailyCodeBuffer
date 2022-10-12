877d2dd0d1cb7f3f94c2e0fc88590a882689474a
lalit <lalit@lalits-MacBook-Pro.local>
2022-10-12 14:39:06 +0530

    Implament deleteDepartmentById API

--- Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboot/tutorial/controller/DepartmentController.java
+++ Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboot/tutorial/controller/DepartmentController.java
@@ -25,4 +25,9 @@ public class DepartmentController {
     public Department fetchDepartmentById(@PathVariable("id") Long departmentId){
         return departmentService.fetchDepartmentById(departmentId);
     }
+    @DeleteMapping("/departments/{id}")
+    public String deleteDepartmentById(@PathVariable("id") Long departmentId){
+        departmentService.deleteDepartmentById(departmentId);
+        return "Department Deleted Successfully!!";
+    }
 }
--- Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboot/tutorial/service/DepartmentService.java
+++ Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboot/tutorial/service/DepartmentService.java
@@ -9,4 +9,6 @@ public interface DepartmentService {
     public List<Department> fetchDepartmentList();
 
     public Department fetchDepartmentById(Long departmentId);
+
+    public void deleteDepartmentById(Long departmentId);
 }
