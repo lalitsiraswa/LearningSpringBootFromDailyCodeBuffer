1590b3fb088dcacc0853bd5a71be5e2e3ad9ab75
lalit <lalit@lalits-MacBook-Pro.local>
2022-10-15 12:35:31 +0530

    Adding Loggers

--- Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboot/tutorial/controller/DepartmentController.java
+++ Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboot/tutorial/controller/DepartmentController.java
@@ -1,8 +1,8 @@
 package com.dailycodebuffer.Springboot.tutorial.controller;
 import com.dailycodebuffer.Springboot.tutorial.entity.Department;
-import com.dailycodebuffer.Springboot.tutorial.repository.DepartmentRepository;
 import com.dailycodebuffer.Springboot.tutorial.service.DepartmentService;
-import com.dailycodebuffer.Springboot.tutorial.service.DepartmentServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,13 +13,18 @@ import java.util.List;
 public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
+
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
 //    @RequestMapping(value = "/departments", method = RequestMethod.POST)
     @PostMapping("/departments")
     public Department saveDepartment(@Valid @RequestBody Department department){
+        LOGGER.info("Inside saveDepartment of DepartmentController.");
         return departmentService.saveDepartment(department);
     }
     @GetMapping("/departments")
     public List<Department> fetchDepartmentList(){
+        LOGGER.info("Inside fetchDepartmentList of DepartmentController.");
         return departmentService.fetchDepartmentList();
     }
     @GetMapping("/departments/{id}")
