cb32174e52ea1e4ab633fd873edb574b70f2cea8
lalit <lalit@lalits-MacBook-Pro.local>
2022-10-16 21:29:28 +0530

    Unit Testing (Service Layer)

---
+++ Spring-boot-tutorial/src/test/java/com/dailycodebuffer/Springboot/tutorial/service/DepartmentServiceTest.java
@@ -0,0 +1,37 @@
+package com.dailycodebuffer.Springboot.tutorial.service;
+import com.dailycodebuffer.Springboot.tutorial.entity.Department;
+import com.dailycodebuffer.Springboot.tutorial.repository.DepartmentRepository;
+import org.junit.jupiter.api.*;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DepartmentServiceTest {
+    @Autowired
+    private DepartmentService departmentService;
+    @MockBean
+    private DepartmentRepository departmentRepository;
+//    @BeforeAll
+    @BeforeEach
+    void setUp() {
+        Department department = Department.builder()
+                .departmentName("IT")
+                .departmentAddress("Ahmedabad")
+                .departmentCode("IT-06")
+                .departmentId(1L)
+                .build();
+        Mockito.when(departmentRepository.findByDepartmentNameIgnoreCase("IT")).thenReturn(department);
+    }
+//    @Disabled // @Disabled annotation will disable that particular Test-Case
+    @Test
+    @DisplayName("Get Data Based On Valid Department Name")
+    public void whenValidDepartmentName_thenDepartmentShouldFound(){
+        String departmentName = "IT";
+        Department found = departmentService.fetchDepartmentByName(departmentName);
+        assertEquals(departmentName, found.getDepartmentName());
+    }
+}
