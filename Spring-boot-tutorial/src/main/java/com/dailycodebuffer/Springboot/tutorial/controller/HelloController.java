8866e4484469c7533d280bf129fd332f1e94f7d2
lalit <lalit@lalits-MacBook-Pro.local>
2022-10-25 13:53:10 +0530

    Adding config in properties file

--- Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboot/tutorial/controller/HelloController.java
+++ Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboot/tutorial/controller/HelloController.java
@@ -1,4 +1,6 @@
 package com.dailycodebuffer.Springboot.tutorial.controller;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -6,9 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+//    This particular value will be fetched from the application.properties
+    @Value("${welcome.message}")
+    private String welcomeMessage;
 //    @RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
     @GetMapping(value = {"/", "/home"})
     public String helloWorld(){
-        return "Welcome to the world!";
+        return welcomeMessage;
     }
 }
