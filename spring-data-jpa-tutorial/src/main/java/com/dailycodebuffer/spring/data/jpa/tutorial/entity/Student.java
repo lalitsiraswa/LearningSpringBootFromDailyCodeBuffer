8cc5cdae36fb9d16f9ff30153b18ce3115a92d77
lalit <lalit@lalits-MacBook-Pro.local>
2022-11-03 15:28:40 +0530

    Different JPA Annotations

--- spring-data-jpa-tutorial/src/main/java/com/dailycodebuffer/spring/data/jpa/tutorial/entity/Student.java
+++ spring-data-jpa-tutorial/src/main/java/com/dailycodebuffer/spring/data/jpa/tutorial/entity/Student.java
@@ -3,19 +3,40 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+
+import javax.persistence.*;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Table(
+        name = "tbl_student",
+        uniqueConstraints = @UniqueConstraint(
+                name = "emailid_unique",
+                columnNames = "email_address" // columnNames = {"email_address", "firstName", lastName} This is how we can pass multiple column names
+        )
+) // This is how we will define the name of the table
 public class Student {
     @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long studentId;
     private String firstName;
     private String lastName;
+
+    @Column(
+            name = "email_address",
+            nullable = false
+    ) // This is how we will define the name of the column
     private String emailId;
     private String guardianName;
     private String guardianEmail;
