6d79a3ace90a546459a9cd386bf8b6f5174517fa
lalit <lalit@lalits-MacBook-Pro.local>
2022-11-08 12:30:37 +0530

    Paging And Sorting (Pagination With Sorting)

--- spring-data-jpa-tutorial/src/test/java/com/dailycodebuffer/spring/data/jpa/tutorial/repository/CourseRepositoryTest.java
+++ spring-data-jpa-tutorial/src/test/java/com/dailycodebuffer/spring/data/jpa/tutorial/repository/CourseRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
@@ -54,6 +55,25 @@ class CourseRepositoryTest {
 //        long totalPages = courseRepository.findAll(thirdPageWithOneRecords).getTotalPages();
         System.out.println("totalPages : " + totalPages);
         System.out.println("totalElements : " + totalElements);
+        System.out.println("courses : " + courses);
+    }
+    @Test
+    public void findAllSorting(){
+        Pageable sortByTitle = PageRequest.of(0, 2, Sort.by("title"));
+        Pageable sortByCreditDesc = PageRequest.of(0, 2, Sort.by("credit").descending());
+        Pageable sortByTitleAndCreditDesc = PageRequest.of(
+                0,
+                2,
+                Sort.by("title")
+                        .descending()
+                        .and(Sort.by("credit").descending()));
+        List<Course> courses = courseRepository.findAll(sortByTitle).getContent();
+        System.out.println("courses : " + courses);
+    }
+    @Test
+    public void printFindByTitleContaining(){
+        Pageable firstPageTenRecord = PageRequest.of(0, 10);
+        List<Course> courses = courseRepository.findByTitleContaining("D", firstPageTenRecord).getContent();
         System.out.println("courses : " + courses);
     }
 }
