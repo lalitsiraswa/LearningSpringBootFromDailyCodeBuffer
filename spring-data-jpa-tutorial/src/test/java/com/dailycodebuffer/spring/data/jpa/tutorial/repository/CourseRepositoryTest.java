a819d8c401aa1c86afd3df2aad5e6fe385fc8c86
lalit <lalit@lalits-MacBook-Pro.local>
2022-11-09 12:25:54 +0530

    JPA Many To Many Relationship(It creates a new third table)

--- spring-data-jpa-tutorial/src/test/java/com/dailycodebuffer/spring/data/jpa/tutorial/repository/CourseRepositoryTest.java
+++ spring-data-jpa-tutorial/src/test/java/com/dailycodebuffer/spring/data/jpa/tutorial/repository/CourseRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.dailycodebuffer.spring.data.jpa.tutorial.repository;
 import com.dailycodebuffer.spring.data.jpa.tutorial.entity.Course;
+import com.dailycodebuffer.spring.data.jpa.tutorial.entity.Student;
 import com.dailycodebuffer.spring.data.jpa.tutorial.entity.Teacher;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -75,5 +76,24 @@ class CourseRepositoryTest {
         Pageable firstPageTenRecord = PageRequest.of(0, 10);
         List<Course> courses = courseRepository.findByTitleContaining("D", firstPageTenRecord).getContent();
         System.out.println("courses : " + courses);
+    }
+    @Test
+    public void saveCourseWithStudentAndTeacher(){
+        Teacher teacher = Teacher.builder()
+                .firstName("Abhinav")
+                .lastName("Sharma")
+                .build();
+        Student student = Student.builder()
+                .firstName("Abhishek")
+                .lastName("Kumar")
+                .emailId("abhishekkumar@gmail.com")
+                .build();
+        Course course = Course.builder()
+                .title("AI")
+                .credit(12)
+                .teacher(teacher)
+                .build();
+        course.addStudents(student);
+        courseRepository.save(course);
     }
 }
