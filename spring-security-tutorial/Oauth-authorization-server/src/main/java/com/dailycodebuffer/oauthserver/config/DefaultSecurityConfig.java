bf2342ccdff9a14ab87699f1ff8ae6437d46aa8d
lalit <lalit@lalits-MacBook-Pro.local>
2022-11-30 21:40:59 +0530

    Adding Configuration In Client

--- spring-security-tutorial/Oauth-authorization-server/src/main/java/com/dailycodebuffer/oauthserver/config/DefaultSecurityConfig.java
+++ spring-security-tutorial/Oauth-authorization-server/src/main/java/com/dailycodebuffer/oauthserver/config/DefaultSecurityConfig.java
@@ -1,4 +1,5 @@
 package com.dailycodebuffer.oauthserver.config;
+import com.dailycodebuffer.oauthserver.service.CustomAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,8 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 public class DefaultSecurityConfig {
+    @Autowired
+    private CustomAuthenticationProvider customAuthenticationProvider;
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -18,5 +21,11 @@ public class DefaultSecurityConfig {
                 )
                 .formLogin(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Autowired
+    public void bindAuthenticationProvider(AuthenticationManagerBuilder authenticationManagerBuilder) {
+        authenticationManagerBuilder
+                .authenticationProvider(customAuthenticationProvider);
     }
 }
--- spring-security-tutorial/spring-security-client/src/main/java/com/dailycodebuffer/client/controller/HelloController.java
+++ spring-security-tutorial/spring-security-client/src/main/java/com/dailycodebuffer/client/controller/HelloController.java
@@ -3,10 +3,12 @@ package com.dailycodebuffer.client.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 public class HelloController {
-    @GetMapping("/hello")
-    public String hello(){
-        return "Hello, Welcome to the world!";
+    @GetMapping("/api/hello")
+    public String hello(Principal principal){
+        return "Hello " + principal.getName() + " Welcome to the world!";
     }
 }
